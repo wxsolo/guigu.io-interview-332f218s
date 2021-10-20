c6e722445386992c909c58772db27b23e01e2233
xing.wu <xing.wu@qunar.com>
2021-10-20 15:52:22 +0800

    fix component name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import PhotoSearch from './pagelet/FlickrPhotosSearch'
+import FlickrPhotosSearch from './pagelet/FlickrPhotosSearch'
 
 function App() {
   return (
     <div className="App">
-      <PhotoSearch />
+      <FlickrPhotosSearch />
     </div>
   );
 }
